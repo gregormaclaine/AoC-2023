dbe9bce10f17cddb762a2638d2d173180f5907c5
Gregor Maclaine <thegregormaclaine@gmail.com>
2023-12-02 00:53:30 +0000

    Fixed bug in asm day 1b and added full solution

--- asm/day-1b.asm
+++ asm/day-1b.asm
@@ -1,8 +1,28 @@
 .data
 
-buffer: .asciiz "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen"
+input_file: .asciiz "/Users/gregormaclaine/Projects/AoC-2023/input/day-1.txt"
+buffer: .space 22000  # 22KB
 
 .text
+
+read_file:
+	li $v0, 13           # Load the system call code for open file
+	la $a0, input_file   # Load the address of the input file name
+	li $a1, 0            # Flag for reading
+	li $a2, 0            # Mode is ignored
+	syscall
+	
+	move $s0, $v0        # Save the file descriptor returned by syscall
+	
+	li $v0, 14           # Load the system call code for reading from file
+	move $a0, $s0        # Load the file descriptor to read
+	la $a1, buffer       # Load the address of the buffer to write into
+	li $a2, 22000         # Read the entire file into the buffer
+	syscall
+	
+	li $v0, 16           # Load the system call for close file
+	move $a0, $s0        # Load the file descriptor to close
+	syscall
 
 li $t0, 0   # Character index
 li $t2, 0   # Running sum
