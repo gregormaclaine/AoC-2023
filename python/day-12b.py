c5d838b3f5fab9b44afdf851e2430a1641dc4ff0
Gregor Maclaine <thegregormaclaine@gmail.com>
2023-12-12 19:21:58 +0000

    Added additional major improvement

--- python/day-12b.py
+++ python/day-12b.py
@@ -14,15 +14,15 @@ def is_valid(springs: str, groups):
         all(a == b for a, b in zip(found_groups, groups))
 
 
-def can_valid(springs: str, groups):
-    pass  # TODO
-
-
 def brute_force(springs: str, groups):
     needed = sum(groups) - springs.count('#')
+
+    if needed < 0:
+        return 0
+
     slots = [i for i, c in enumerate(springs) if c == '?']
     valid_count = 0
-    print(f'  - checking {comb(len(slots), needed)} combinations')
+    # print(f'  - checking {comb(len(slots), needed)} combinations')
     for chosen in combinations(slots, needed):
         result = [(c if c != '?' else '.') for c in springs]
         for i in chosen:
@@ -32,35 +32,31 @@ def brute_force(springs: str, groups):
     return valid_count
 
 
-def calc_possiblities(springs: str, groups):
-    if len(groups) == 0:
-        return 1
+def brute_force_by_group(springs: str, groups):
+    if sum(groups) - springs.count('#') < 0:
+        return 0
 
-    if springs.startswith('.') or springs.endswith('.'):
-        return calc_possiblities(springs.lstrip('.').rstrip('.'), groups)
+    wide_slots = [g for g in springs.split('.') if len(g)]
+    if len(wide_slots) == 1:
+        return brute_force(springs, groups)
 
-    if springs.startswith('#'):
-        g = groups.pop(0)
-        return calc_possiblities(springs[g + 1:], groups)
+    total = 0
 
-    if springs.endswith('#'):
-        g = groups.pop()
-        return calc_possiblities(springs[:-(g + 1)], groups)
+    first_slot_count = 0
+    for first_slot_count in range(len(groups) + 1):
+        if sum(groups[:first_slot_count]) + len(groups[:first_slot_count]) - 1 > len(wide_slots[0]):
+            break
 
-    if '#' in springs[:groups[0]]:
-        pos = springs.index('#')
-        if pos < groups[0] - 1:
-            springs = springs[:pos] + '#' * \
-                (groups[0] - pos) + springs[groups[0]:]
-            return calc_possiblities(springs, groups)
+        total += brute_force(wide_slots[0], groups[:first_slot_count]) * \
+            calc_possiblities_loop(
+                '.'.join(wide_slots[1:]), groups[first_slot_count:])
 
-    print(springs, groups)
-    return brute_force(springs, groups)
+    return total
 
 
-def calc_possiblities_loop(springs: str, groups):
+def calc_possiblities_loop(springs: str, groups, first=False):
     while True:
-        if len(groups) == 0:
+        if len(groups) == 0 or springs.count('?') == 0:
             return 1
 
         if springs.startswith('.') or springs.endswith('.'):
@@ -95,12 +91,28 @@ def calc_possiblities_loop(springs: str, groups):
                 if old != springs:
                     continue
 
+        wide_slots = [g for g in springs.split('.') if len(g)]
+        if len(wide_slots):
+            if len(wide_slots[0]) <= groups[0]:
+                springs = '.'.join(wide_slots[1:])
+                if len(wide_slots[0]) == groups[0]:
+                    groups = groups[1:]
+                continue
+
+            if len(wide_slots[-1]) <= groups[-1]:
+                springs = '.'.join(wide_slots[:-1])
+                if len(wide_slots[-1]) == groups[-1]:
+                    groups = groups[:-1]
+                continue
+
         break
 
-    print(springs, groups)
-    return brute_force(springs, groups)
+    if first:
+        print(springs, '\n', '  -', groups)
+
+    return brute_force_by_group(springs, groups)
 
 
 with open('input/day-12.txt', 'r') as f:
     lines = f.read().split('\n')
-    print(sum(calc_possiblities_loop(*parse_line(l)) for l in lines))
+    print(sum(calc_possiblities_loop(*parse_line(l), True) for l in lines))
